24cf2353387aa0529141698e2c48e188169b3bd8
lkush <l.kushnariova@yandex.ru>
2018-05-01 00:10:56 +0300

    Add rules screen

--- match-match-game/js/app.js
+++ match-match-game/js/app.js
@@ -1,13 +1,62 @@
-class Game {
-    constructor () {
-    }
-}
-
 const cardCount = 12
 let matchedCardCount = 0
 let gameTime
 
-const cardsContainer = document.querySelector('.cards')
+const rulesTemplate = `
+<main class="main-content">
+    <section class="about-game">
+        <div class="description">
+            <h1 class="heading description__heading heading_inverted">Match match game</h1>
+            <p class="description__text">Match match game is a counter game where the object is to find pairs.</p>
+            <p class="description__text">When the game begins, all pictures are hidden.</p>
+        </div>
+        <div class="rules">
+            <h2 class="heading heading_inverted">How to play</h2>
+            <ol class="rules__list">
+                <li class="rules__item">Select two cards to try to match the pictures</li>
+                <li class="rules__item">If you match the pictures you can do again</li>
+                <li class="rules__item">If they don't match it is the computer turn them.</li>
+                <li class="rules__item">The player that finds all pairs wins!</li>
+                <li class="rules__item">Have fun!</li>
+            </ol>
+        </div>
+    </section>
+    <section class="game-options">
+        <div class="card-shirts">
+            <h2 class="heading card-shirts__heading">Select card shirts</h2>
+            <div class="card-shirts__wrapper">
+                <div class="card shirt shirt__geometric"></div>
+                <div class="card shirt shirt__dotted"></div>
+                <div class="card shirt shirt__tissue"></div>
+            </div>
+        </div>
+        <div class="difficulty">
+            <h2 class="heading difficulty__heading">Select difficulty level</h2>
+            <div class="difficulty-btn-group">
+                <div class="btn btn_outline difficulty__btn">4x3</div>
+                <div class="btn btn_outline difficulty__btn">4x4</div>
+                <div class="btn btn_outline difficulty__btn">4x5</div>
+            </div>
+        </div>
+        <button class="btn start-game__btn">Start game</button>
+    </section>
+</main>
+`
+
+const gameTemplate = `
+<main class="play-field">
+    <div class="play__options">
+        <button class="btn play__options-btn play__options-btn_new-game">New game</button>
+        <div class="play__options-timer"></div>
+    </div>
+
+    <div class="cards"></div>
+
+    <p class="copyright">
+        Drawings by <a class="copyright__link" target="_blank" href="https://dribbble.com/Necklaceee">NOZ Creative</a>
+    </p>
+</main>
+`
 
 const cardTemplate = `
     <div class="card card__item">
@@ -19,6 +68,7 @@ const cardTemplate = `
         </div>
     </div>
 `
+
 const cardImages = [
     'apple.png',
     'apricot.png',
@@ -32,6 +82,21 @@ const cardImages = [
     'plum.png'
 ]
 
+showRules()
+
+function showRules () {
+    document.querySelector('body').innerHTML = rulesTemplate
+    document.querySelector('.start-game__btn').addEventListener('click', showGame)
+}
+
+function showGame () {
+    document.body.innerHTML = gameTemplate
+    createCards()
+    startTimer()
+    document.querySelector('.play__options-btn_new-game').addEventListener('click', showRules)
+    document.addEventListener('click', flipCard)
+}
+
 function createCards () {
     const cardsData = []
 
@@ -41,7 +106,7 @@ function createCards () {
     }
     cardsData.sort((a, b) => a.order - b.order)
 
-    cardsContainer.innerHTML = cardsData
+    document.querySelector('.cards').innerHTML = cardsData
         .map(item => cardTemplate.replace('{filename}', item.filename))
         .join('')
 }
@@ -60,7 +125,6 @@ function startTimer () {
     }, 1000)
 }
 
-document.addEventListener('click', flipCard)
 let openedCard
 
 function flipCard (event) {
@@ -103,6 +167,3 @@ function delayUnflip (card1, card2) {
         card2.classList.remove('flipped')
     }, 1000)
 }
-
-createCards()
-startTimer()
