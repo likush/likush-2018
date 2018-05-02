f8a1046b6cd35c005642765d0b5d28bfdc45d032
lkush <l.kushnariova@yandex.ru>
2018-05-03 00:08:40 +0300

    Load user profile on new game start

--- match-match-game/js/app.js
+++ match-match-game/js/app.js
@@ -11,7 +11,7 @@ document.querySelector('.registration-form').addEventListener('submit', function
 
 document.addEventListener('click', function (ev) {
     if (ev.target.matches('.play__options-btn_new-game')) {
-        showRules()
+        showRules(loadProfile())
     }
 })
 
