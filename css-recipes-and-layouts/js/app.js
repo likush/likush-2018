f556885aee12e257e5a0c3a664391cd606f7db67
lkush <l.kushnariova@yandex.ru>
2018-05-06 19:10:52 +0300

    Final implementation

--- css-recipes-and-layouts/js/app.js
+++ css-recipes-and-layouts/js/app.js
@@ -4,21 +4,18 @@ function showHiddenContent (ev) {
     const selectedEl = ev.target
     if (selectedEl.closest('[data-content]')) {
         const dataEl = selectedEl.dataset.content
-        console.log(selectedEl, dataEl, prevEl)
+
         document.querySelector('.container')
             .querySelector(`[data-content="${dataEl}"]`)
-            .classList.toggle('s-hidden')
-        if (prevEl) {
+            .classList.toggle('visible')
+        if (prevEl && prevEl !== dataEl) {
             document.querySelector('.container')
                 .querySelector(`[data-content="${prevEl}"]`)
-                .classList.add('s-hidden')
+                .classList.remove('visible')
+
         }
         prevEl = dataEl
     }
 }
 
 document.addEventListener('click', showHiddenContent)
-//
-// document.querySelector('.navbar__collapsed-btn_nav').addEventListener('click', function (ev) {
-//     document.querySelector('.navbar__nav-list').style.display = 'flex'
-// })
